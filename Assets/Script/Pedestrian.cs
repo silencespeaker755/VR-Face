6c327cadd4528cb7ac9ee65fad24492055f52fb5
yishuolin <yslin.eecs06@nctu.edu.tw>
2022-01-04 14:03:31 +0800

    fix: change the mesh of Sunny to generic in the last scene to avoid penetration

--- Assets/Script/Pedestrian.cs
+++ Assets/Script/Pedestrian.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Pedestrian : MonoBehaviour
 {
-    public float speed = 1.3f;
+    public float speed = 1f;
+    public GameObject door;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position -= Vector3.forward * Time.deltaTime * speed;
+        //transform.position -= Vector3.forward * Time.deltaTime * speed;
+        Vector3 target = door.transform.position;
+        target.x -= 5;
+        target.y = 0;
+
+        transform.LookAt(door.transform);
+        float FollowSpeed = .02f;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, FollowSpeed);
     }
 }
